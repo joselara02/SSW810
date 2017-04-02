1bc3b1a3fc6d321a8532197471218a0570aa8697
Jose Lara <Jose@Joses-MacBook-Pro.local>
2017-04-02 18:52:10 -0400

    adding Major Class, adding missing courses to student table,  and refatoring of code

--- Lara_Week10.py
+++ Lara_Week10.py
@@ -20,6 +20,7 @@ class Student:
         self.CWID = id
         self.major = major
         self.courses = defaultdict(str)
+        self.missing_courses = list()
 
     def assign_grade(self, course, grade):
         self.courses[course] = grade
@@ -29,6 +30,12 @@ class Student:
 
     def get_grades(self, course):
         return self.courses.get(course)
+
+    def set_missing_courses(self, courses):
+        self.missing_courses = courses
+
+    def get_missing_courses(self):
+        return self.missing_courses
 
 
 class Instructor:
@@ -50,13 +57,45 @@ class Instructor:
         return self.courses.get(course)
 
 
+class Major:
+    '''stores the require courses for SFEN and SYEN students'''
+    def __init__(self):
+        self.required_software_engineering = set()
+        self.required_system_engineering = set()
+
+    def set_major_requirements(self, major, course):
+        '''sets the major required by students'''
+        if major == 'SFEN':
+            self.required_software_engineering.add(course)
+        elif major == 'SYEN':
+            self.required_system_engineering.add(course)
+        else:
+            print('{} does not belong to {} major'.format(course, major))
+
+
+    def get_missing_courses(self, student):
+        '''get the missing courses for the major required by students'''
+        for key, value in student.items():
+            course_taken = set([x for x in student[key].get_courses()])
+            student_major = student[key].major
+
+            if student_major == 'SFEN':
+                remainder = list(self.required_software_engineering - course_taken)
+                student[key].set_missing_courses(remainder)
+            if student_major == 'SYEN':
+                remainder = list(self.required_system_engineering - course_taken)
+                student[key].set_missing_courses(remainder)
+
+        return None
+
+
 class Repository:
     '''defines a object of Repository type. It holds all students, instructors and grades.'''
 
     def read_data(self, file_name):
         '''Read files for students and instructor data'''
         try:
-            file_opened = open(file_name, "r")
+            open(file_name, "r")
 
         except FileNotFoundError:
             print(file_name, "file cannot be opened!")
@@ -65,31 +104,32 @@ class Repository:
             print('Please check that file is not corrupted.')
 
         else:
+            with open(file_name, 'r') as file_opened:
+                # reading data from file
+                each_line = file_opened.readlines()
 
-            # reading data from file
-            each_line = file_opened.readlines()
+                if len(each_line) == 0:
+                    raise ValueError('file is empty')
+                else:
+                    dict_data = defaultdict(lambda: defaultdict(str))
+                    for line in each_line:
+                        CWID, name, dept = line.strip('\n').split('\t')
 
-            if len(each_line) == 0:
-                raise ValueError('file is empty')
-            else:
-                dict_data = defaultdict(lambda: defaultdict(str))
-                for line in each_line:
-                    CWID, name, dept = line.strip('\n').split('\t')
+                        if file_name == 'students.txt':
+                            dict_data.update({CWID: Student(CWID, name, dept)})
 
-                    if file_name == 'students.txt':
-                        dict_data.update({CWID: Student(CWID, name, dept)})
+                        elif file_name == 'instructors.txt':
+                            dict_data.update({CWID: Instructor(CWID, name, dept)})
 
-                    elif file_name == 'instructors.txt':
-                        dict_data.update({CWID: Instructor(CWID, name, dept)})
+                        else:
+                            return None
 
-                    else:
-                        return None
-
-                return dict_data
+                    return dict_data
 
     def read_grades_file(self, file_name, student_data, instructor_data):
+        '''Read files for grades for students and the courses instructor teach data'''
         try:
-            file_opened = open(file_name, "r")
+            open(file_name, "r")
 
         except FileNotFoundError:
             print(file_name, "file cannot be opened!")
@@ -99,21 +139,46 @@ class Repository:
 
         else:
             # reading data from file
-            each_line = file_opened.readlines()
+            with open(file_name, "r") as file_opened:
+                each_line = file_opened.readlines()
 
-            if len(each_line) == 0:
-                raise ValueError('file is empty')
-            else:
-                for line in each_line:
-                    student_id, course, grade, instructor_id = line.strip('\n').split('\t')
+                each_line.sort()
 
-                    student_data[student_id].assign_grade(course, grade)
+                if len(each_line) == 0:
+                    raise ValueError('file is empty')
+                else:
+                    for line in each_line:
+                        student_id, course, grade, instructor_id = line.strip('\n').split('\t')
 
-                    instructor_data[instructor_id].courses_taught(course)
+                        student_data[student_id].assign_grade(course, grade)
 
+                        instructor_data[instructor_id].courses_taught(course)
+
+    def read_majors(self, file_name, major_class):
+        '''Read the required courses for each students major '''
+        try:
+            open(file_name, 'r')
+        except IOError:
+            print('There was en error processing the {} file'.format(file_name))
+        except FileNotFoundError:
+            print('{} file does not exist'.format(file_name))
+        else:
+            with open(file_name, 'r') as file_opened:
+                each_line = file_opened.readlines()
+
+                if len(each_line) == 0:
+                    raise ValueError('File is Empty')
+                else:
+                    for line in each_line:
+                        major, course = line.strip('\n').split('\t')
+
+                        major_class.set_major_requirements(major, course)
+
+                    return major_class
 
 
     def instructor_table(self, data):
+        '''Displays a pretty Table for instructors data'''
         instructor_table = PrettyTable(["CWID", "Name", "Dept", 'Course', 'Students'])
 
         for x in data:
@@ -125,9 +190,10 @@ class Repository:
         print(instructor_table)
 
     def student_table(self, data):
-        student_table = PrettyTable(["CWID", "Name", "Completed Courses"])
+        '''Displays a pretty Table for students data'''
+        student_table = PrettyTable(["CWID", "Name", "Completed Courses", "Missing Courses"])
         for x in data:
-            student_table.add_row([data[x].CWID, data[x].name, [x for x in data[x].get_courses()]])
+            student_table.add_row([data[x].CWID, data[x].name, [x for x in data[x].get_courses()], data[x].get_missing_courses()])
 
         print("Student Table")
         print(student_table)
@@ -135,11 +201,12 @@ class Repository:
 
 def main():
     repo = Repository()
-
+    majors = Major()
     student_data = repo.read_data('students.txt')
     instructor_data = repo.read_data('instructors.txt')
     repo.read_grades_file('grades.txt', student_data, instructor_data)
-
+    repo.read_majors('majors.txt', majors)
+    majors.get_missing_courses(student_data)
     repo.instructor_table(instructor_data)
     repo.student_table(student_data)
 
